b27f1e535851c4694b5acca684cd4da775f8faba
Rahul Darasing Chavan <46101641+rahulGPJ@users.noreply.github.com>
2022-11-11 18:32:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+// C Program to Perform Arithmetic Operations Using Functions
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+    num1 = 9;
+    num2 = 1;
+    printf("9 + 1 = %d", num1+num2);
+    return 0;
+}
